ebf131c5b190bdffb723e08743ff46ba4716592d
Hannan66-sys <12hannan07@gmail.com>
2025-06-25 11:52:49 +0530

    transaction

--- src/app/ecommerce/transations/transations.component.ts
+++ src/app/ecommerce/transations/transations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-transations',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transations.component.scss']
 })
 export class TransationsComponent implements OnInit {
+  transactions: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.loadTransactions();
   }
 
+  loadTransactions() {
+    const url = 'https://cybercloudapp.com/wp-json/wc/v3/orders';
+
+    const username = 'ck_dd111222ce2c0914e75dc284afff6a080243a2b4';
+    const password = 'cs_31cfcfe1e7ac08abafcf197a0d651e32a0758987';
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Basic ' + btoa(username + ':' + password)
+    });
+
+    this.http.get<any[]>(url, { headers }).subscribe(
+      data => {
+        console.log(data);
+        this.transactions = data;
+      },
+      error => {
+        console.error('Failed to fetch transactions', error);
+      }
+    );
+  }
 }
