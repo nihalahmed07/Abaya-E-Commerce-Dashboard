2a6c3316175be734e2ae2e590098e88351af1a78
shahith2003 <mshahith21@gmail.com>
2025-06-27 16:45:28 +0530

    Added category button functionality

--- src/app/ecommerce/categories/categories.component.ts
+++ src/app/ecommerce/categories/categories.component.ts
@@ -1,22 +1,6 @@
-/* import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
-})
-export class CategoriesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-} */
-
-
- import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/categories.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
@@ -25,15 +9,71 @@ import { CategoryService } from 'src/app/services/categories.service';
 })
 export class CategoriesComponent implements OnInit {
   categories: any[] = [];
+  newCategory = {
+    name: '',
+    slug: '',
+    parent: 0,
+    description: ''
+  };
+  editingCategoryId: number | null = null;
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(private categoryService: CategoryService, private router: Router) {}
 
   ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories() {
     this.categoryService.getCategories().subscribe({
       next: (data) => this.categories = data,
       error: (err) => console.error('Failed to load categories', err)
     });
   }
+
+  addCategory() {
+    this.categoryService.addCategory(this.newCategory).subscribe({
+      next: () => {
+        this.loadCategories();
+        this.newCategory = { name: '', slug: '', parent: 0, description: '' };
+      },
+      error: (err) => console.error('Failed to add category', err)
+    });
+  }
+
+  editCategory(category: any) {
+    this.editingCategoryId = category.id;
+    this.newCategory = { ...category };
+  }
+
+  updateCategory() {
+    if (this.editingCategoryId !== null) {
+      this.categoryService.updateCategory(this.editingCategoryId, this.newCategory).subscribe({
+        next: () => {
+          this.loadCategories();
+          this.editingCategoryId = null;
+          this.newCategory = { name: '', slug: '', parent: 0, description: '' };
+        },
+        error: (err) => console.error('Failed to update category', err)
+      });
+    }
+  }
+
+  deleteCategory(id: number): void {
+    if (confirm('Are you sure you want to delete this category?')) {
+      this.categoryService.deleteCategory(id).subscribe({
+        next: () => {
+          this.categories = this.categories.filter(c => c.id !== id);
+          alert('Category deleted successfully.');
+        },
+        error: (err) => {
+          console.error('Delete failed:', err);
+          alert('Failed to delete category.');
+        }
+      });
+    }
+  }
+
+  viewCategory(id: number) {
+    this.router.navigate(['/categories', id]);
+  }
 }
-
-
