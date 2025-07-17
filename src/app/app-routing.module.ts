3d8888fc4ba58e2f3ab971d9699c443d809bb9c9
Keerthi-U <keerthiiachu173@gmail.com>
2025-07-17 17:36:57 +0530

    invoice page and pdf page fecth and design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,14 +15,13 @@ import { CONTENT_ROUTES } from './shared/routes/content-layout.routes';
 import { SignupWithHeaderFooterComponent } from './auth/signup-with-header-footer/signup-with-header-footer.component';
 import { SigninWithHeaderFooterComponent } from './auth/signin-with-header-footer/signin-with-header-footer.component';
 
-
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard/sales',
     pathMatch: 'full',
   },
-   {
+  {
     path: '',
     component: FullLayoutComponent,
     data: { title: 'full Views' },
@@ -30,33 +29,32 @@ const routes: Routes = [
       ...Full_ROUTES,
       {
         path: 'application/invoice/:id',
-        component: InvoiceComponent
+        component: InvoiceComponent,
       },
       {
         path: 'order-details/:id',
-        component: OrdersDetailsComponent
+        component: OrdersDetailsComponent,
       }
     ]
   },
-  // { path: '', component: FullLayoutComponent, data: { title: 'full Views' }, children: Full_ROUTES },
-  { path: '', component: ContentLayoutComponent, data: { title: 'content Views' }, children: CONTENT_ROUTES },
-  // Sign Up and Sign In Routes
-  { path: 'signup-with-header-footer', component: SignupWithHeaderFooterComponent },
-  { path: 'signin-with-header-footer', component: SigninWithHeaderFooterComponent },
-
-  { path: '**', redirectTo: 'auth/signup-with-header-footer' },
-
-
-  { 
-    path: 'order-details/:id', 
-    component: OrdersDetailsComponent 
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    data: { title: 'content Views' },
+    children: CONTENT_ROUTES
   },
-  // { 
-  //   path: 'invoice/:id', 
-  //   component: InvoiceComponent 
-  // },
-    { path: '**', redirectTo: 'auth/signup-with-header-footer' },
-   { path: 'application/invoice/:id', component: InvoiceComponent },  // Ensure this is correct
+  {
+    path: 'signup-with-header-footer',
+    component: SignupWithHeaderFooterComponent
+  },
+  {
+    path: 'signin-with-header-footer',
+    component: SigninWithHeaderFooterComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'signup-with-header-footer'
+  }
 ];
 
 @NgModule({
